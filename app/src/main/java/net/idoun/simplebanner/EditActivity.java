4f14852bcb1efb9c0ccb4ae8caa9d235ef84a451
idoun <idoun21@gmail.com>
2016-03-26 20:28:45 +0900

    Initiate project

---
+++ app/src/main/java/net/idoun/simplebanner/EditActivity.java
@@ -0,0 +1,13 @@
+package net.idoun.simplebanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(net.idoun.simplebanner.R.layout.activity_edit);
+    }
+}
