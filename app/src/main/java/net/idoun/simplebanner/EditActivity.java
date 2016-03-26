2e94a265aa2ac128387849b079f33e18c3bcf348
idoun <idoun21@gmail.com>
2016-03-26 21:41:54 +0900

    Edit activity added. Every widget will share the same text.

--- app/src/main/java/net/idoun/simplebanner/EditActivity.java
+++ app/src/main/java/net/idoun/simplebanner/EditActivity.java
@@ -1,13 +1,68 @@
 package net.idoun.simplebanner;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditActivity extends AppCompatActivity {
+
+    public static final String PREF_USER_TEXT = "USER_TEXT";
+    private static final String TAG = "EditActivity";
+    public static final String ACTION_APPWIDGET_UPDATE = "net.idoun.intent.action.APPWIDGET_UPDATE";
+
+    private EditText inputEditText;
+
+    private String text = "";
+
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(net.idoun.simplebanner.R.layout.activity_edit);
+        setContentView(R.layout.activity_edit);
+
+        inputEditText = (EditText)findViewById(R.id.edit_input);
+        Button cancelButton = (Button)findViewById(R.id.cancel_button);
+        Button saveButton = (Button)findViewById(R.id.save_button);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentText = inputEditText.getText().toString();
+                Log.d(TAG, "onClick: " + currentText);
+                if (!currentText.isEmpty() && !text.equals(currentText)) {
+                    prefs.edit().putString(PREF_USER_TEXT, currentText).apply();
+                    updateMyWidgets(EditActivity.this);
+                    finish();
+                }
+            }
+        });
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.contains(PREF_USER_TEXT)) {
+            text = prefs.getString(PREF_USER_TEXT, "");
+
+            inputEditText.setText(text);
+        }
+    }
+
+    private void updateMyWidgets(Context context) {
+        Intent updateIntent = new Intent();
+        updateIntent.setAction(ACTION_APPWIDGET_UPDATE);
+        context.sendBroadcast(updateIntent);
     }
 }
--- app/src/main/java/net/idoun/simplebanner/SimpleBannerAppWidgetProvider.java
+++ app/src/main/java/net/idoun/simplebanner/SimpleBannerAppWidgetProvider.java
@@ -1,18 +1,50 @@
 package net.idoun.simplebanner;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.widget.RemoteViews;
 
 public class SimpleBannerAppWidgetProvider extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+
+        if (EditActivity.ACTION_APPWIDGET_UPDATE.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            ComponentName provider = new ComponentName(context, SimpleBannerAppWidgetProvider.class);
+            int[] ids = appWidgetManager.getAppWidgetIds(provider);
+            onUpdate(context, appWidgetManager, ids);
+        }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        for (int appWidgetId : appWidgetIds) {
+            Intent intent = new Intent(context, EditActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.banner_appwidget);
+            views.setOnClickPendingIntent(R.id.background, pendingIntent);
+
+            if (prefs.contains(EditActivity.PREF_USER_TEXT)) {
+                String text = prefs.getString(EditActivity.PREF_USER_TEXT, "");
+
+                if (!text.isEmpty()) {
+                    views.setTextViewText(R.id.text, text);
+                }
+            }
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
 }
