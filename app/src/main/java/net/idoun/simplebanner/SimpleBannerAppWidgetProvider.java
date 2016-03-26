063079c327826d73c27cc417aa41cde9e9eed8a7
idoun <idoun21@gmail.com>
2016-03-26 20:21:16 +0900

    Initiate widget layout

---
+++ app/src/main/java/net/idoun/simplebanner/SimpleBannerAppWidgetProvider.java
@@ -0,0 +1,18 @@
+package net.idoun.simplebanner;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class SimpleBannerAppWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
